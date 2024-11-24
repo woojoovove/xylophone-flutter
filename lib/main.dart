4da1656395bc4661e7a7d2543a46425b64349a12
woojoovove <ujinjeong55@gmail.com>
2024-11-24 10:51:04 +0900

    [add] 7 buttons with each sound

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,82 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-              child: TextButton(
-            onPressed: () async {
-              final player = AudioPlayer();
-              final duration = await player.setUrl('asset:/assets/note1.wav');
-              player.play();
-            },
-            child: Text('click me'),
-          )),
+            child: Column(
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    final duration =
+                        await player.setUrl('asset:/assets/note1.wav');
+                    player.play();
+                  },
+                  child: Text(''),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    final duration =
+                        await player.setUrl('asset:/assets/note2.wav');
+                    player.play();
+                  },
+                  child: Text(''),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    final duration =
+                        await player.setUrl('asset:/assets/note3.wav');
+                    player.play();
+                  },
+                  child: Text(''),
+                ),
+                TextButton(
+                  style:
+                      TextButton.styleFrom(backgroundColor: Colors.lightGreen),
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    final duration =
+                        await player.setUrl('asset:/assets/note4.wav');
+                    player.play();
+                  },
+                  child: Text(''),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    final duration =
+                        await player.setUrl('asset:/assets/note5.wav');
+                    player.play();
+                  },
+                  child: Text(''),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    final duration =
+                        await player.setUrl('asset:/assets/note6.wav');
+                    player.play();
+                  },
+                  child: Text(''),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    final duration =
+                        await player.setUrl('asset:/assets/note7.wav');
+                    player.play();
+                  },
+                  child: Text(''),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
